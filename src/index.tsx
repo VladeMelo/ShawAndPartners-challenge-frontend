8545082fa7f24cc3e17e3af1de871bf6c3c024d9
VladeMelo <vlademirjos@hotmail.com>
2020-11-10 15:02:47 -0300

    Removing Perfil page

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
-import Perfil from './pages/Perfil';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Switch>
         <Route path='/' exact component={Main} />
-        <Route path='/perfil' component={Perfil} />
       </Switch>
     </BrowserRouter>
   </React.StrictMode>,
